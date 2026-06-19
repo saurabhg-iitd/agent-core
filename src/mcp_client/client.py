f20a3357cdd198d2cf5fc485256e61f4ff74dd0f
casentars <saurabh.gautam@wheelseye.com>
2026-06-19 14:02:32 +0530

    fix: mcp fix3

--- src/mcp_client/client.py
+++ src/mcp_client/client.py
@@ -1,7 +1,7 @@
 from mcp.client.sse import sse_client
 from strands.tools.mcp.mcp_client import MCPClient
 
-LITELLM_MCP_ENDPOINT = "http://litellm.prod-we.com/mcp"
+LITELLM_MCP_ENDPOINT = "http://litellm.prod-we.com/mcp/"
 LITELLM_API_KEY = "Bearer sk-ETFcYN4ay5SmXs5B1NEXpg"
 
 def get_litellm_mcp_client() -> MCPClient:
