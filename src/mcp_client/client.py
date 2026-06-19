eb01c6c973494acd82dc2a7ac6ae6b343f7f3ee8
casentars <saurabh.gautam@wheelseye.com>
2026-06-19 14:10:22 +0530

    fix: mcp 4

--- src/mcp_client/client.py
+++ src/mcp_client/client.py
@@ -1,11 +1,11 @@
-from mcp.client.sse import sse_client
+from mcp.client.streamable_http import streamablehttp_client
 from strands.tools.mcp.mcp_client import MCPClient
 
-LITELLM_MCP_ENDPOINT = "http://litellm.prod-we.com/mcp/"
+LITELLM_MCP_ENDPOINT = "http://litellm.prod-we.com/mcp"
 LITELLM_API_KEY = "Bearer sk-ETFcYN4ay5SmXs5B1NEXpg"
 
 def get_litellm_mcp_client() -> MCPClient:
-    return MCPClient(lambda: sse_client(
+    return MCPClient(lambda: streamablehttp_client(
         LITELLM_MCP_ENDPOINT,
         headers={"x-litellm-api-key": LITELLM_API_KEY},
     ))
