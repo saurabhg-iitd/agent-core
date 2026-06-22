cfdf17d85fade6f68a1b069cbf4194bcfdc7767d
casentars <saurabh.gautam@wheelseye.com>
2026-06-22 16:53:15 +0530

    fix: changed role

--- scripts/setup_online_eval.py
+++ scripts/setup_online_eval.py
@@ -3,6 +3,8 @@ from bedrock_agentcore_starter_toolkit import Evaluation
 
 os.environ.setdefault("AWS_DEFAULT_REGION", "ap-south-1")
 
+EXECUTION_ROLE_ARN = "arn:aws:iam::489284938287:role/bedrock-read-access"
+
 eval_client = Evaluation()
 
 config = eval_client.create_online_config(
@@ -15,7 +17,7 @@ config = eval_client.create_online_config(
         "Builtin.ToolSelectionAccuracy",
     ],
     config_description="Continuous evaluation of wheelseyedemo agent",
-    auto_create_execution_role=True,
+    execution_role_arn=EXECUTION_ROLE_ARN,
     enable_on_create=True,
 )
 
